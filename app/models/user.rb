6dabd837a22313ec75917a7fd029da16bbf2ff6f
Daniyal Rao <raothegreat_dani_91@hotmail.com>
2016-07-11 17:56:43 -0400

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+  mount_uploader :avatar, AvatarUploader
+
+  def full_name
+  	"#{first_name} #{last_name}"
+  end
+end
